7149bdc72a447a9022e106d7dfd11d69c0561bf3
kongzhipeng <704002390@qq.com>
2017-05-03 20:50:54 +0800

    add

--- js/S-project.js
+++ js/S-project.js
@@ -4,19 +4,38 @@
  * @date    2017-04-08 13:16:38
  * @version $Id$
  */
-$(document).ready(function(){
-	$("input.ui-page-input-c").text("");
-	$("button.ui-page-bt-kaisa-jiami").click(function(){
-		 $("input.ui-page-input-c").val($("input.ui-page-input-m").val()+$(".ui-page-input-key").val);
+$(init)
+function init(){
+	//凯撒加密
+	$(".ui-page-bt-jiami").click(function(){
+		var key=parseInt($(".ui-page-input-key").val());
+		var str=$(".ui-page-input-m").val();
+		var kaisai=new Array();
+		for(i=0;i<$(str.length);i++){
+			var num=str[i].charCodeAt();
+			if(num>=65 && num<=90)
+				num=(num-65+key)%26+65;
+			else if(num>=97 && num<=122)
+				num=(num-97+key)%26+97;
+			kaisai[i]=String.fromCharCode(num);
+
+		}
+		$(".ui-page-input-c").html("+kaisai+");
 	});
-});
 
+	//滑动页
+	$(".ui-page-box").hide();
+	$(".ui-way-cell:eq(0)").css("background-color","#ffffff");
+	$(".ui-page-box:eq(0)").fadeIn(500);
+	$(".ui-way-cell").click(function(){
+		var number=$(this).index();
+		$(this).css("background-color","#ffffff").siblings(this).css("background-color","#ccc");
+		$(".ui-page-box:eq("+number+")").fadeIn(500).siblings(".ui-page-box").fadeOut();
+	});
 
-// $(init)
-// function init(){
-// 
-// 
-// 
-// }
-// 
+	$(".ui-page-load").click(function(){
+
+	});
+}
+
 
